e0acd62c22c04d720fc9b4fde79e9e8d457c9237
Exonor515 <exonor.work@gmail.com>
2024-05-09 01:28:52 +0300

    abc

--- vayan.js
+++ vayan.js
@@ -1,1 +1,45 @@
 const lexa = "NE pidoras";
+
+//symbol string
+
+let fruit = "apple";
+const hero = "qopa";
+
+console.log(fruit, hero);
+
+fruit = "orange";
+//hero = "pudge"; //error
+
+console.log(fruit);
+
+//number bigint
+
+let num = 1.232435;
+let num1 = 1000;
+console.log(num);
+
+//boolean
+
+const isLexaPidoras = false;
+const isDimasPidoras = true;
+
+console.log(isLexaPidoras, isDimasPidoras);
+
+//null undefined
+
+let vasyan;
+
+console.log(vasyan);
+
+vasyan = null;
+
+console.log(vasyan);
+
+const banana = false;
+const govno = 228;
+
+console.log(govno + banana);
+
+//+ - / % *
+
+console.log(200 + govno);
