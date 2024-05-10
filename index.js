d71e35b5609cc72ea89e9ca82b38947ed0539d8c
Enotovoker <nikc12311@gmail.com>
2024-05-10 23:33:17 +0300

    govno

---
+++ index.js
@@ -0,0 +1,31 @@
+let num1 = 1000;
+let num2 = 500;
+let num3 = 100;
+
+const result = (num1 * num2) / num3;
+console.log(result);
+
+const string1 = "Vasyan";
+const string2 = "Molodec!";
+const string3 = "NeMolodec!";
+console.log(string1 + " " + string2);
+
+console.log(string1 + " " + undefined);
+
+console.log(string1 + true);
+
+console.log(null - undefined);
+
+console.log(10 - undefined);
+
+let vasyan = "Vasyan";
+let isVasyanWon = undefined;
+
+if (isVasyanWon) {
+  vasyan = vasyan + string2;
+} else if (isVasyanWon === null) {
+  vasyan = vasyan + string3;
+} else {
+  vasyan = vasyan + vasyan;
+}
+console.log(vasyan);
